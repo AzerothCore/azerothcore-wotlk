84a8f3c1cb81ee0cb12f6f89415aee0c6739f9a7
Patrick Müssig <patrick.muessig@gmx.de>
2025-02-04 16:39:20 +0100

    Include more ReputationSources

--- src/server/scripts/OutdoorPvP/OutdoorPvPSI.cpp
+++ src/server/scripts/OutdoorPvP/OutdoorPvPSI.cpp
@@ -25,6 +25,7 @@
 #include "OutdoorPvPScript.h"
 #include "Player.h"
 #include "ReputationMgr.h"
+#include "ScriptMgr.h"
 #include "Transport.h"
 #include "World.h"
 #include "WorldPacket.h"
@@ -114,7 +115,9 @@ bool OutdoorPvPSI::HandleAreaTrigger(Player* player, uint32 trigger)
                 // add 19 honor
                 player->RewardHonor(nullptr, 1, 19);
                 // add 20 cenarion circle repu
-                player->GetReputationMgr().ModifyReputation(sFactionStore.LookupEntry(609), 20.f);
+                float reputation = 20.f;
+                ScriptMgr::instance()->OnBeforePlayerReputationChange(player, 609, reputation, ReputationSource::PvP);
+                player->GetReputationMgr().ModifyReputation(sFactionStore.LookupEntry(609), reputation);
                 // complete quest
                 player->KilledMonsterCredit(SI_TURNIN_QUEST_CM_A);
             }
@@ -140,7 +143,9 @@ bool OutdoorPvPSI::HandleAreaTrigger(Player* player, uint32 trigger)
                 // add 19 honor
                 player->RewardHonor(nullptr, 1, 19);
                 // add 20 cenarion circle repu
-                player->GetReputationMgr().ModifyReputation(sFactionStore.LookupEntry(609), 20.f);
+                float reputation = 20.f;
+                ScriptMgr::instance()->OnBeforePlayerReputationChange(player, 609, reputation, ReputationSource::PvP);
+                player->GetReputationMgr().ModifyReputation(sFactionStore.LookupEntry(609), reputation);
                 // complete quest
                 player->KilledMonsterCredit(SI_TURNIN_QUEST_CM_H);
             }
